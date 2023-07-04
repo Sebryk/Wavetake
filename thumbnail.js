8b6959a1ee134976868b20de75f159542cb13d23
Sergei Brykalov <106953297+sebryk@users.noreply.github.com>
2023-07-04 10:26:07 +0400

    Add files via upload

---
+++ thumbnail.js
@@ -0,0 +1,31 @@
+import { data } from './data.js';
+
+export const thumbnail = () => {
+  const thumbnailGallery = document.querySelector('.thumbnail');
+  const popupWindow = document.querySelector('.popup');
+  let thumbnailItem;
+  data.forEach(
+    el =>
+      (thumbnailGallery.innerHTML += thumbnailItem =
+        `
+      <div class="thumbnail__item" id="${el.id}">
+        <picture>
+        <source  class="thumbnail__img"  srcset=${el.imgUrlWebp} type="image/webp" >
+        <img class="thumbnail__img" src=${el.imgUrlJpg} alt="${el.name} project cover image" type="image/jpeg" >
+        </picture>
+        <div class="thumbnail__content">
+          <h4 class="thumbnail__title">${el.name}</h4>
+          <p class="thumbnail__desc">${el.services}</p>
+        </div>
+      </div>`)
+  );
+
+  data.forEach(el =>
+    popupWindow.insertAdjacentHTML(
+      'beforeend',
+      `<iframe class="popup__video" id="item-${el.id}"
+      title="vimeo-player" src="${el.videoUrl}"
+      allowfullscreen style="display: none;"></iframe> `
+    )
+  );
+};
