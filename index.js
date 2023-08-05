3611db0dea50491f77a7b0e66cb2bdbb7d52d3c3
Sergei Brykalov <106953297+sebryk@users.noreply.github.com>
2023-08-05 22:59:14 +0400

    Add Audio Player

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { burgerMenu } from './js-modules/burger.js';
 import { popUp } from './js-modules/popup.js';
 import { showMoreBtn } from './js-modules/show-more-btn.js';
 import { thumbnail } from './js-modules/thumbnail.js';
+import { audioPlayer } from './js-modules/audio-player.js';
 
 scroll();
 
@@ -17,6 +18,10 @@ thumbnail();
 /* ----------------------------- Button "Show More"---------------------------- */
 
 showMoreBtn();
+
+/* ------------------------------ AUdio Player ------------------------------ */
+
+audioPlayer();
 
 /* ----------------------------- Pop Up"---------------------------- */
 
---
+++ scroll.js
@@ -0,0 +1,43 @@
+export const scroll = () => {
+  const navBar = document.querySelector('.header__nav');
+  const scrollIcon = document.querySelector('.header__scroll');
+  const navListItems = document.querySelectorAll('.nav__list-item');
+  const sections = document.querySelectorAll('[data-id="section"]');
+
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          navListItems.forEach(link => {
+            let id = link.dataset.id;
+            if (id === entry.target.id) {
+              link.classList.add('nav__list-item--active');
+            } else {
+              link.classList.remove('nav__list-item--active');
+            }
+          });
+        }
+      });
+    },
+    {
+      threshold: 0.6,
+    }
+  );
+
+  sections.forEach(section => observer.observe(section));
+
+  /* -------- Scroll Icon Behavior and Navigation Shadow When Scrolling ------- */
+
+  window.scrollTo(0, 0);
+  window.addEventListener('scroll', () => {
+    const scroll = window.scrollY;
+
+    if (scroll > 0) {
+      navBar.classList.add('header__nav--scroll');
+      scrollIcon.style.left = '-40px';
+    } else if (scroll < 30) {
+      navBar.classList.remove('header__nav--scroll');
+      scrollIcon.style.left = '30px';
+    }
+  });
+};
